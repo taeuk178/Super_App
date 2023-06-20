4f865741e0668273585c65a0858f97c5eb0a729a
taeuk178 <nous178@naver.com>
2023-06-21 06:35:18 +0900

    feat: 메인화면 작업

--- Super_App/ViewController.swift
+++ Super_App/ViewController.swift
@@ -7,13 +7,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    private let mainStackView: UIStackView = {
+        $0.axis = .vertical
+        $0.spacing = 25
+        $0.alignment = .fill
+        $0.distribution = .fillEqually
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIStackView())
+    
+    private let uikitLabel: UIButton = {
+        $0.setTitle("UIKit", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        $0.layer.cornerRadius = 15
+        $0.layer.borderColor = UIColor.black.cgColor
+        $0.layer.borderWidth = 3
+        return $0
+    }(UIButton(type: .custom))
+    
+    private let swiftUILabel: UIButton = {
+        $0.setTitle("SwiftUI", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        $0.layer.cornerRadius = 15
+        $0.layer.borderColor = UIColor.black.cgColor
+        $0.layer.borderWidth = 3
+        return $0
+    }(UIButton(type: .custom))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        setLayer()
+        
+        uikitLabel.addTarget(self, action: #selector(pushUIKit(_:)), for: .touchUpInside)
+        swiftUILabel.addTarget(self, action: #selector(pushSwiftUI(_:)), for: .touchUpInside)
+    }
+    
+    @objc
+    private func pushUIKit(_ sender: UIButton) {
+        
+    }
+    
+    @objc
+    private func pushSwiftUI(_ sender: UIButton) {
+        
     }
 
-
+    private func setLayer() {
+        
+        view.addSubview(mainStackView)
+        mainStackView.addArrangedSubview(uikitLabel)
+        mainStackView.addArrangedSubview(swiftUILabel)
+        
+        let size = UIScreen.main.bounds.size
+        mainStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            .isActive = true
+        mainStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            .isActive = true
+        mainStackView.widthAnchor.constraint(equalToConstant: size.width / 3)
+            .isActive = true
+        mainStackView.heightAnchor.constraint(equalToConstant: size.height / 5)
+            .isActive = true
+    }
+    
 }
 
